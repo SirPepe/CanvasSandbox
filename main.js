5d044f330350120a7b2e3b7205a5b70745e5e443
Peter Kröner <peter@peterkroener.de>
2012-04-15 16:53:11 +0200

    Unterstützung für requestAnimationFrame()
    Signed-off-by: Peter Kröner <peter@peterkroener.de>

--- main.js
+++ main.js
@@ -3,9 +3,21 @@
 
 // Public vars and functions
 var canvas, context, findPos;
+
 Math.toRad = function(x){
 	return (x * Math.PI) / 180;
 };
+
+window.requestAnimationFrame = window.requestAnimationFrame || (function(){
+	return window.requestAnimationFrame        ||
+			window.webkitRequestAnimationFrame ||
+			window.mozRequestAnimationFrame    ||
+			window.oRequestAnimationFrame      ||
+			window.msRequestAnimationFrame     ||
+			function(callback){
+				window.setTimeout(callback, 1000 / 60);
+			};
+})();
 
 window.addEventListener('DOMContentLoaded', function(){
 "use strict";
